e5ea89e420e6c3bc26b74f4e33e03d633be2cd2e
Eliott Davidson <ulthos@gmail.com>
2020-08-31 09:16:45 -0700

    updated resume

--- pages/resume.js
+++ pages/resume.js
@@ -7,7 +7,7 @@ export default function Resume() {
         <>
             <NavBar />
             <main>
-                <Document file="../documents/ed_resume_08-25-2020.pdf">
+                <Document file="../documents/ed_resume_08-31-2020.pdf">
                     <Page className={styles.centered} pageNumber={1} />
                 </Document>
             </main>
